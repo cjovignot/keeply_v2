2a165fd9810ac6bd55b3c05fe829b392fc6f3239
cjovignot <cosme.jovignot@epitech.eu>
2025-12-02 23:37:50 +0100

    [FIX]

--- server/api/routes/auth.ts
+++ server/api/routes/auth.ts
@@ -97,19 +97,19 @@ router.get("/google/callback", async (req, res) => {
       expiresIn: "7d",
     });
 
-    // res.cookie("token", token, {
-    //   httpOnly: true,
-    //   secure: false,
-    //   sameSite: "lax",
-    //   maxAge: 7 * 24 * 60 * 60 * 1000,
-    // });
-
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
+    // res.cookie("token", token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production",
+    //   sameSite: "lax",
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    // });
 
     res.redirect(`${process.env.FRONTEND_URL}/auth/success`);
   } catch (err) {
