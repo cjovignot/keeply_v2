73893090d5d0407f6b0d0880a52b81eed069bd21
cjovignot <cosme.jovignot@epitech.eu>
2025-12-03 22:20:08 +0100

    [VERCEL] Serverless

--- vercel-server.ts
+++ vercel-server.ts
@@ -1,4 +1,3 @@
 import serverless from "serverless-http";
-import app from "./server/api/index";
-
+import app from "./server/api/index.js"; // ✅ extension .js obligatoire en ES Modules
 export const handler = serverless(app);
