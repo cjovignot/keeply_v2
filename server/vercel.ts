a6fe5f56fa2ca6bf6f18d565ebc6b4162bc67afa
cjovignot <cosme.jovignot@epitech.eu>
2025-12-04 19:16:20 +0100

    [FIX]

--- server/vercel.ts
+++ server/vercel.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../server/api/index.js';
+import app from './api/index';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return (app as any)(req, res);
