2c56a8d21998dc6ad25c9d184ba3132cba0394f8
cjovignot <cosme.jovignot@epitech.eu>
2025-12-04 12:56:36 +0100

    [FIX]

--- vercel-server.ts
+++ vercel-server.ts
@@ -1,3 +1,3 @@
 import serverless from "serverless-http";
-import app from "./server/api/index.ts"; // PAS de dist
+import app from "./server/api/index.js"; // PAS de dist
 export const handler = serverless(app);
